bda6d16810289e14621e5a46f8a085f9da039f9f
WAKU-TAKE-A <waku-take-a@ymail.ne.jp>
2023-01-14 20:46:15 +0900

    windows10以上を対象にしているのでwhereを使います

--- 1_Install_Ruby_x64.bat
+++ 1_Install_Ruby_x64.bat
@@ -1,9 +1,7 @@
 ReadMe1.jpg
 ReadMe2.jpg
 
-dir /A-D /B /S | findstr "rubyinstaller[^\\]*$" > tmp.txt
-for /f "usebackq" %%i in (tmp.txt) do set FN_INSTALLER=%%i
-del tmp.txt
+for /f "usebackq" %%i in (`where rubyinstaller*`) do set FN_INSTALLER=%%i
 
 echo Run "%FN_INSTALLER%" > InstallLog.txt
 %FN_INSTALLER%
